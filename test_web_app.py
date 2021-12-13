ece4ee56c3d28c6cf3c73428fbae1c02453fc270
Duramann <theo.dura@efrei.net>
2021-12-13 12:16:30 +0100

    Stress test, accuracy tests added

--- test_web_app.py
+++ test_web_app.py
@@ -1,10 +1,13 @@
-import requests
 import web_app
 import pytest
 import nltk
 import vaderSentiment
 import string
+import pandas as pd
+import time
+import random
 
+## Create a pytest fixture that configure the application for testing. 
 @pytest.fixture
 def client():
     app = web_app.app
@@ -14,24 +17,63 @@ def client():
         with app.test_client() as client:
             yield client
 
+## Test if the site is working for "/" route and "/predict" route.
 def test_status(client):
     mp = client.get("/")
     assert mp.status_code == 200
     red = client.get("/predict")
     assert mp.status_code == 200
+    predicted = client.post('/predict', data={"sentence":"Today was a good day!"})
+    assert predicted.status_code == 200
 
+## Test if the site returns the correct answer when sending sentence to analyse.
 def test_predic(client):
-    pred1 = client.post('/predict', data={"sentence":"Today was a good day!"})
-    assert b'positive' in pred1.data
-    pred2 = client.post('/predict', data={"sentence":"Today was a bad day!"})
-    assert b'negative' in pred2.data
-    pred3 = client.post('/predict', data={"sentence":"Dogs are animals."})
-    assert b'neutral' in pred3.data
+    pred_pos = client.post('/predict', data={"sentence":"Today was a good day!"})
+    assert b'positive' in pred_pos.data
+    pred_neg = client.post('/predict', data={"sentence":"Today was a bad day!"})
+    assert b'negative' in pred_neg.data
+    pred_neut = client.post('/predict', data={"sentence":"Dogs are animals."})
+    assert b'neutral' in pred_neut.data
+    pred_fr = client.post('/predict', data={"sentence":"Cette phrase est en Français"})
+    assert b'neutral' in pred_fr.data
+    pred_int = client.post('/predict', data={"sentence":"12 54 52 35"})
+    assert b'neutral' in pred_int.data
 
-#def test_stress(client):
-#    time = []
-#    for i in range(1000):
-#        res = client.get('/')
-#        time.append(res.elapsed.total_seconds()*1000)
-#    avg_time = sm(time)/len(time)
-#    assert avg_time < 100
+## Test if the average response time of the home page is below 100ms.
+def test_stress_home(client):
+    recorded_time = []
+    for i in range(1000):
+        start = time.time()
+        client.get('/')
+        end = time.time()
+        recorded_time.append(start-end*1000)
+    avg_time = sum(recorded_time)/len(recorded_time)
+    assert avg_time < 100
+
+## Test if the average response time of a prediction is below 100ms.
+def test_stress_prediction(client):
+    recorded_time = []
+    data = pd.read_csv('data/labelled_sentence.csv', sep=";", header=0)
+    for i in range(1000):
+        index = random.randint(0, len(data)-1)
+        start = time.time()
+        client.post('/predict', data={"sentence":data['sentence'][index]})
+        end = time.time()
+        recorded_time.append(start-end*1000)
+    avg_time = sum(recorded_time)/len(recorded_time)
+    assert avg_time < 100
+
+## Test if the accuracy of our back-end model is above or equal to 70%.
+def test_accuracy(client):
+    correct_values = 0
+    data = pd.read_csv('data/labelled_sentence.csv', sep=";", header=0)
+    for i in range(len(data)):
+        pred = client.post('/predict', data={"sentence":data['sentence'][i]})
+        if b'positive' in pred.data and data['label'][i]==1:
+            correct_values +=1
+        if b'negative' in pred.data and data['label'][i]==0:
+            correct_values+=1
+    accuracy = correct_values/len(data)
+    assert accuracy >= 0.7
+        
+
